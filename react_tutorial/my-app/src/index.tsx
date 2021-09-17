1ec7304e5eaf6ab318d79a03875e4ce252011046
haznai <aitakprojects@gmail.com>
2021-09-17 15:31:50 +0300

    turned square class component to function component

--- react_tutorial/my-app/src/index.tsx
+++ react_tutorial/my-app/src/index.tsx
@@ -2,27 +2,48 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component<
-  { value?: number },
-  { value?: number | string }
-> {
-  state = this.props;
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({ value: "x" })}>
-        {this.state.value}
-      </button>
-    );
-  }
+// function component
+function Square(props: { value: null | string; onClick: () => void }) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
-class Board extends React.Component {
+class Board extends React.Component<
+  {}, // empty props
+  { squares: (string | null)[]; xIsNext: boolean }
+> {
+  // general state of the board
+  // gets passed down to individual squares
+  state = { squares: Array(9).fill(null), xIsNext: true };
+
+  // method that gets passed down to the squares
+  // gets called by the square when clicked
+  handleclick(i: number) {
+    // return early if square has been set before
+    if (this.state.squares[i] != null) {
+      return;
+    }
+
+    const squares = this.state.squares.slice();
+    this.state.xIsNext ? (squares[i] = "X") : (squares[i] = "O");
+    // update the state and toggle xIsNext
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
+  }
+
   renderSquare(i: number) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleclick(i)}
+      />
+    );
   }
 
   render() {
-    const status = "Next player: X";
+    const status = `Next player: ${this.state.xIsNext ? "X" : "0"}`;
 
     return (
       <div>
